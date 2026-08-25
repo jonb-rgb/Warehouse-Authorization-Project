4b80c2d93af22d50e08d99da303ccc3bfb7c41c5
Jonathan <jon1707b@gmail.com>
2026-08-25 05:13:05 -0700

    Add Warehouse info and employee details

---
+++ Warehouse_info.py
@@ -0,0 +1,28 @@
+roles = [
+    "Associate",
+    "Janitor",
+    "Supervisor",
+    "Secretary",
+    "Customer Support",
+    "Security",
+    "IT",
+    "Frontend Manager",
+    "Operations Manager",
+    "General Manager",
+]
+
+floors = [
+    1,
+    2,
+    3,
+]
+
+areas = [
+    "Frontend",
+    "Backend",
+    "Offices",
+    "Storage",
+    "Server Room",
+    "Security Room",
+    "Managers Office"
+]
--- main.py
+++ main.py
@@ -1,1 +1,116 @@
 print("Warehouse-Authorization-Project")
+
+employees = [
+    {
+        "ID": 1,
+        "name": "Ryan",
+        "role": "General Manager",
+        "floors": [1, 2, 3],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Offices",
+            "Storage",
+            "Server Room",
+            "Managers Office"
+        ]
+    },
+    {
+        "ID": 2,
+        "name": "John",
+        "role": "Operations Manager",
+        "floors": [1, 2],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Offices",
+            "Storage"
+        ]
+    },
+    {
+        "ID": 3,
+        "name": "Sophia",
+        "role": "Frontend Manager",
+        "floors": [1, 2, 3],
+        "areas": [
+            "Frontend",
+            "Offices",
+            "Storage",
+            "Server Room"
+        ]
+    },
+    {
+        "ID": 4,
+        "name": "Richie",
+        "role": "IT",
+        "floors": [1, 2, 3],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Server Room",
+            "Offices"
+        ]
+    },
+    {
+        "ID": 5,
+        "name": "Jacob",
+        "role": "Security",
+        "floors": [1],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Security Room"
+        ]
+    },
+    {
+        "ID": 6,
+        "name": "Emily",
+        "role": "Customer Support",
+        "floors": [1, 2],
+        "areas": [
+            "Frontend",
+            "Offices"
+        ]
+    },
+    {
+        "ID": 7,
+        "name": "Josh",
+        "role": "Secretary",
+        "floors": [1, 2],
+        "areas": [
+            "Frontend"
+        ]
+    },
+    {
+        "ID": 8,
+        "name": "Mia",
+        "role": "Supervisor",
+        "floors": [1, 2],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Offices"
+        ]
+    },
+    {
+        "ID": 9,
+        "name": "Hector",
+        "role": "Janitor",
+        "floors": [1, 2],
+        "areas": [
+            "Frontend",
+            "Backend",
+            "Offices",
+            "Storage"
+        ]
+    },
+    {
+        "ID": 10,
+        "name": "Maria",
+        "role": "Associate",
+        "floors": [1],
+        "areas": [
+            "Backend"
+        ]
+    }
+]
